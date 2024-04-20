8c53e64cb3305e498aaec846d9b6052f4fb28214
matt.barr <matt@mysidewalk.com>
2024-04-20 09:53:35 -0500

    Added clustering

--- 2024/lab-01/cluster-search.py
+++ 2024/lab-01/cluster-search.py
@@ -11,6 +11,10 @@
 # Import necessary libraries
 from txtai import embeddings
 import word_list
+import sentence_transformers
+import umap
+import pandas as pd
+import plotly.express as px
 
 # %%
 embedding_config = {"path": "avsolatorio/GIST-small-Embedding-v0", "content": True}
@@ -27,3 +31,19 @@ embedding_engine.search("royal", limit=5)
 # %%
 # Search for items related to 'basketball'
 embedding_engine.search("basketball", limit=5)
+
+# %%
+# Embed the word list
+encoder = sentence_transformers.SentenceTransformer("avsolatorio/GIST-small-Embedding-v0")
+embedded_word_list = encoder.encode(word_list.all_words)
+reducer = umap.UMAP(n_components=2, random_state=42)
+reduced_embedded_word_list = reducer.fit_transform(embedded_word_list)
+reduced_words_df = pd.DataFrame(reduced_embedded_word_list, columns=["x", "y"])
+reduced_words_df["word"] = word_list.all_words
+
+# %%
+# Plot the reduced word embeddings
+fig = px.scatter(reduced_words_df, x="x", y="y", hover_data=["word"])
+fig.update_layout(title="Word Embeddings Visualization", xaxis_title="X", yaxis_title="Y", legend_title="Words")
+fig.update_traces(marker_size=5)
+fig.show()
