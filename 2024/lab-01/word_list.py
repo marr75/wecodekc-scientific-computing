582a2621c8f48bdcbba2d7533c5c5aa96d9fb329
matt.barr <matt@mysidewalk.com>
2024-04-20 08:58:35 -0500

    Added word list

---
+++ 2024/lab-01/word_list.py
@@ -0,0 +1,223 @@
+athletes = """
+LeBron James
+Serena Williams
+Tom Brady
+Simone Biles
+Steph Curry
+Megan Rapinoe
+Patrick Mahomes
+Naomi Osaka
+Mike Trout
+Chloe Kim
+Zion Williamson
+Alex Morgan
+Shaun White
+Christian Pulisic
+Venus Williams
+Mookie Betts
+Katie Ledecky
+Bryce Harper
+Sydney McLaughlin
+Giannis Antetokounmpo
+Coco Gauff
+Trevor Lawrence
+Aaron Judge
+Carli Lloyd
+Caeleb Dressel
+""".strip().split()
+
+dishes = """
+Chicken Parmesan
+Beef Stroganoff
+Vegetable Stir Fry
+Spaghetti Bolognese
+Chicken Caesar Salad
+Fish Tacos
+Vegetarian Chili
+Shrimp Scampi
+Pulled Pork Sandwich
+Ratatouille
+Chicken Tikka Masala
+Quinoa Salad with Roasted Vegetables
+Lasagna
+Beef Tacos
+Grilled Salmon with Asparagus
+Macaroni and Cheese
+Philly Cheesesteak
+Pad Thai
+Fajitas
+Chicken Alfredo Pasta
+Falafel Wrap
+French Onion Soup
+Eggplant Parmesan
+Beef Bourguignon
+Greek Salad
+""".strip().split()
+
+actors = """
+Zendaya
+Tom Holland
+Millie Bobby Brown
+Timothée Chalamet
+Florence Pugh
+Dwayne Johnson
+Emma Watson
+Chris Hemsworth
+Selena Gomez
+Michael B. Jordan
+Gal Gadot
+Adam Driver
+Brie Larson
+John Boyega
+Saoirse Ronan
+Robert Downey Jr.
+Scarlett Johansson
+Jason Momoa
+Jennifer Lawrence
+Chris Evans
+Anya Taylor-Joy
+Finn Wolfhard
+Hailee Steinfeld
+Tom Hiddleston
+Margot Robbie
+""".strip().split()
+
+music_artists = """
+Billie Eilish
+Taylor Swift
+Drake
+Ariana Grande
+BTS
+Olivia Rodrigo
+The Weeknd
+Dua Lipa
+Post Malone
+Travis Scott
+Harry Styles
+Doja Cat
+Ed Sheeran
+Cardi B
+Kendrick Lamar
+Shawn Mendes
+Selena Gomez
+Lil Nas X
+Justin Bieber
+Bad Bunny
+Khalid
+Megan Thee Stallion
+SZA
+Halsey
+J Balvin
+""".strip().split()
+
+historical_figures = """
+Nelson Mandela
+Mahatma Gandhi
+Martin Luther King Jr.
+Malcolm X
+Harriet Tubman
+Frida Kahlo
+Confucius
+Rosa Parks
+Che Guevara
+Emperor Haile Selassie
+Marcus Garvey
+Cleopatra
+Sun Yat-sen
+Yaa Asantewaa
+Toussaint L'Ouverture
+Shaka Zulu
+Rabindranath Tagore
+Genghis Khan
+Sitting Bull
+Benazir Bhutto
+Moctezuma II
+Haile Selassie
+Patrice Lumumba
+Mansa Musa
+Sarojini Naidu
+""".strip().split()
+
+jobs = """
+Software Developer
+Nurse
+Teacher
+Civil Engineer
+Graphic Designer
+Chef
+Mechanic
+Architect
+Accountant
+Pharmacist
+Journalist
+Lawyer
+Pilot
+Scientist
+Photographer
+Rapper
+Baseball Player
+Basketball Player
+Actor
+Music Producer
+Dancer
+Fashion Designer
+Veterinarian
+Social Worker
+Electrician
+""".strip().split()
+
+countries = """
+United States
+Canada
+Brazil
+United Kingdom
+France
+Germany
+Italy
+Spain
+Japan
+China
+India
+Russia
+Australia
+Mexico
+South Africa
+Egypt
+Nigeria
+Kenya
+Saudi Arabia
+Turkey
+South Korea
+Indonesia
+Thailand
+New Zealand
+Argentina
+""".strip().split()
+
+colors = """
+Red
+Blue
+Green
+Yellow
+Orange
+Purple
+Black
+White
+Pink
+Gray
+Brown
+Crimson
+Teal
+Turquoise
+Magenta
+Lime
+Gold
+Silver
+Maroon
+Navy
+Olive
+Cyan
+Mauve
+Beige
+Indigo
+""".strip().split()
